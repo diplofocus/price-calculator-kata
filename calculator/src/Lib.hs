dae9aa11a8887af71832d392faaf8a5676b2aca6
Petar Saulic <petar.saulic@tradecore.com>
2019-04-17 21:12:28 +0200

    TAX

--- calculator/src/Lib.hs
+++ calculator/src/Lib.hs
@@ -1,6 +1,27 @@
+{-#LANGUAGE ScopedTypeVariables#-}
+
 module Lib
-    ( someFunc
-    ) where
+  ( someFunc
+  ) where
+
+import Text.Printf (printf)
+
+data Product = Product
+  { name :: String
+  , upc :: Int
+  , price :: Double
+  } deriving (Show)
+
+sampleProduct :: Product
+sampleProduct = Product "a" 12345 20.25;
+
+tax :: Double
+tax = 0.2
+
+format :: Double -> String
+format = printf "%.2f"
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+    putStrLn $ "Price before tax: " ++ (format $ price sampleProduct)
+    putStrLn $ "Price before tax: " ++ (format $ (price sampleProduct * (1 + tax)))
