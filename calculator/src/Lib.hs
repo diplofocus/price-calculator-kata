afa5941158bfe88c681da47b64fda14cbb1c9d7f
Petar Saulic <petar.saulic@tradecore.com>
2019-04-18 12:04:58 +0200

    Add tests.

--- calculator/src/Lib.hs
+++ calculator/src/Lib.hs
@@ -1,31 +1,22 @@
 module Lib
-  ( someFunc
+  ( Tax
+  , Product(..)
+  , (|#|)
+  , format
   ) where
 
 import Text.Printf (printf)
 
+type Tax = Double
 data Product = Product
   { name :: String
   , upc :: Int
   , price :: Double
   } deriving (Show)
 
-sampleProduct :: Product
-sampleProduct = Product "a" 12345 20.25;
-
-tax :: Double
-tax = 0.2
-
-discount :: Double
-discount = 0.15
 
 (|#|) :: Double -> Product -> Double
-(|#|) percentage = (*) percentage . price
+(|#|) percentage = (flip (.)) price $ flip (*) percentage
 
 format :: Double -> String
-format = printf "%.2f"
-
-someFunc :: IO ()
-someFunc = do
-    putStrLn $ "Base price: " ++ (format $ price sampleProduct)
-    putStrLn $ "Price with tax and discount: " ++ (format $ ((1 + tax - discount) |#| sampleProduct))
+format = (++ "$") . printf "%.2f"
--- calculator/test/Spec.hs
+++ calculator/test/Spec.hs
@@ -1,2 +1,27 @@
+import Test.Hspec
+import Lib
+
+sampleProduct :: Product
+sampleProduct = Product "a" 12345 20.25;
+
+tax, tax2 :: Double
+tax = 0.2
+tax2 = 0.21
+
+discount :: Double
+discount = 0.15
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "Lib.displayPrice" $ do
+    describe "TAX" $ do
+      it "reports the price of the product" $ do
+        (format . price) sampleProduct `shouldBe` "20.25$"
+
+      it "reports the taxed price of the product" $ do
+        format ((1 + tax) |#| sampleProduct) `shouldBe` "24.30$"
+        format ((1 + tax2) |#| sampleProduct) `shouldBe` "24.50$"
+
+    describe "DISCOUNT" $ do
+      it "reports the total price of the product, with a discount" $ do
+        format ((1 + tax - discount) |#| sampleProduct) `shouldBe` "21.26$"
